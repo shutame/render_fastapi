a4c06b6969c21cfbe6f876704382fe5005448dfd
TamegaiShuta <shu_tamegai@outlook.jp>
2025-06-13 11:27:58 +0900

    init omikuji

--- main.py
+++ main.py
@@ -12,3 +12,20 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/omikuji")
+def omikuji():
+    import random
+    fortunes = [
+        "大吉",
+        "中吉",
+        "小吉",
+        "吉",
+        "半吉",
+        "末吉",
+        "末小吉",
+        "凶",
+        "小凶",
+        "大凶"
+    ]
+    return {"fortune": random.choice(fortunes)}
